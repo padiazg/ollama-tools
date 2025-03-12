0712c24e49a891f256dc28f1af2019948cf31172
Patricio Diaz <padiazg@gmail.com>
2025-03-12 15:03:31 -0300

    WIP: implementing Test_modelsInfoReader

--- internals/models/models_list_test.go
+++ internals/models/models_list_test.go
@@ -11,6 +11,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type testTags struct {
+	body   string
+	models []string
+}
+
+var (
+	tagsList = map[string]testTags{
+		"one-model": {
+			body:   `{"models":[{"name":"deepseek-r1:7b","model":"deepseek-r1:7b","modified_at":"2025-03-11T15:37:14.423620816-03:00","size":4683075271,"digest":"0a8c266910232fd3291e71e5ba1e058cc5af9d411192cf88b6d30e92b6e73163","details":{"parent_model":"","format":"gguf","family":"qwen2","families":["qwen2"],"parameter_size":"7.6B","quantization_level":"Q4_K_M"}}]}`,
+			models: []string{"deepseek-r1:7b"},
+		},
+		"two-models": {
+			body:   `{"models":[{"name":"deepseek-r1:7b","model":"deepseek-r1:7b","modified_at":"2025-03-11T15:37:14.423620816-03:00","size":4683075271,"digest":"0a8c266910232fd3291e71e5ba1e058cc5af9d411192cf88b6d30e92b6e73163","details":{"parent_model":"","format":"gguf","family":"qwen2","families":["qwen2"],"parameter_size":"7.6B","quantization_level":"Q4_K_M"}},{"name":"qwen2.5-coder:7b","model":"qwen2.5-coder:7b","modified_at":"2025-02-26T13:02:11.408112713-03:00","size":4683087519,"digest":"2b0496514337a3d5901f1d253d01726c890b721e891335a56d6e08cedf3e2cb0","details":{"parent_model":"","format":"gguf","family":"qwen2","families":["qwen2"],"parameter_size":"7.6B","quantization_level":"Q4_K_M"}}]}`,
+			models: []string{"deepseek-r1:7b", "qwen2.5-coder:7b"},
+		},
+	}
+)
+
 type DryRunTransport struct {
 	http.RoundTripper
 	RoundTripFn func(r *http.Request) (*http.Response, error)
@@ -94,14 +112,15 @@ func Test_modelsInfoGenerator(t *testing.T) {
 
 							res = &http.Response{
 								StatusCode: http.StatusOK,
-								Body:       io.NopCloser(strings.NewReader(`{"models":[{"name":"deepseek-r1:7b","model":"deepseek-r1:7b","modified_at":"2025-03-11T15:37:14.423620816-03:00","size":4683075271,"digest":"0a8c266910232fd3291e71e5ba1e058cc5af9d411192cf88b6d30e92b6e73163","details":{"parent_model":"","format":"gguf","family":"qwen2","families":["qwen2"],"parameter_size":"7.6B","quantization_level":"Q4_K_M"}}]}`)),
+								// Body:       io.NopCloser(strings.NewReader(`{"models":[{"name":"deepseek-r1:7b","model":"deepseek-r1:7b","modified_at":"2025-03-11T15:37:14.423620816-03:00","size":4683075271,"digest":"0a8c266910232fd3291e71e5ba1e058cc5af9d411192cf88b6d30e92b6e73163","details":{"parent_model":"","format":"gguf","family":"qwen2","families":["qwen2"],"parameter_size":"7.6B","quantization_level":"Q4_K_M"}}]}`)),
+								Body: io.NopCloser(strings.NewReader(tagsList["one-model"].body)),
 							}
 
 							return res, err
 						},
 					},
 				},
-				check: checkModels([]string{"deepseek-r1:7b"}),
+				check: checkModels(tagsList["one-model"].models),
 			},
 			{
 				name: "two-models",
@@ -116,14 +135,14 @@ func Test_modelsInfoGenerator(t *testing.T) {
 
 							res = &http.Response{
 								StatusCode: http.StatusOK,
-								Body:       io.NopCloser(strings.NewReader(`{"models":[{"name":"deepseek-r1:7b","model":"deepseek-r1:7b","modified_at":"2025-03-11T15:37:14.423620816-03:00","size":4683075271,"digest":"0a8c266910232fd3291e71e5ba1e058cc5af9d411192cf88b6d30e92b6e73163","details":{"parent_model":"","format":"gguf","family":"qwen2","families":["qwen2"],"parameter_size":"7.6B","quantization_level":"Q4_K_M"}},{"name":"qwen2.5-coder:7b","model":"qwen2.5-coder:7b","modified_at":"2025-02-26T13:02:11.408112713-03:00","size":4683087519,"digest":"2b0496514337a3d5901f1d253d01726c890b721e891335a56d6e08cedf3e2cb0","details":{"parent_model":"","format":"gguf","family":"qwen2","families":["qwen2"],"parameter_size":"7.6B","quantization_level":"Q4_K_M"}}]}`)),
+								Body:       io.NopCloser(strings.NewReader(tagsList["two-models"].body)),
 							}
 
 							return res, err
 						},
 					},
 				},
-				check: checkModels([]string{"deepseek-r1:7b", "qwen2.5-coder:7b"}),
+				check: checkModels(tagsList["two-models"].models),
 			},
 			{
 				name: "specific-models",
@@ -147,6 +166,7 @@ func Test_modelsInfoGenerator(t *testing.T) {
 			},
 		}
 	)
+
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
@@ -182,25 +202,49 @@ func Test_modelsInfoGenerator(t *testing.T) {
 // 	}
 // }
 
-// func Test_modelsInfoReader(t *testing.T) {
-// 	type args struct {
-// 		next nextFn
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 		want chan nextData
-// 	}{
-// 		// TODO: Add test cases.
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			if got := modelsInfoReader(tt.args.next); !reflect.DeepEqual(got, tt.want) {
-// 				t.Errorf("modelsInfoReader() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
+func Test_modelsInfoReader(t *testing.T) {
+	type checkModelsInfoReader func(t *testing.T, pending <-chan nextData)
+
+	var (
+		checkModelName = func(model_name string) checkModelsInfoReader {
+			return func(t *testing.T, pending <-chan nextData) {
+				t.Helper()
+				fetched := make(chan nextData)
+
+				go func() {
+					for {
+						data, ok := <-pending
+						if !ok {
+							break
+						}
+						fetched <- data
+					}
+				}()
+
+				value := <-fetched
+				assert.Equal(t, model_name, value)
+			}
+		}
+
+		tests = []struct {
+			name       string
+			model_name string
+			check      checkModelsInfoReader
+		}{
+			{
+				name:       "success-one-model",
+				model_name: "test",
+				check:      checkModelName("test"),
+			},
+		}
+	)
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+
+		})
+	}
+}
 
 // func Test_modelsInfoFetcher(t *testing.T) {
 // 	type args struct {
